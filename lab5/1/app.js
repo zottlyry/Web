11bc2465e5d7e73d68b83ebffd707440d5ed6aa8
zottlyry <mar1ya322@yandex.ru>
2025-06-10 16:40:07 +0300

    Add files via upload

---
+++ lab5/1/app.js
@@ -0,0 +1,9 @@
+
+let age = "12";
+let name = "John";
+
+
+let output = <span>{name} is {age} years old</span>;
+
+
+ReactDOM.render(output, document.querySelector("#myDiv"));  
---
+++ lab5/2/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customer = {
+	first_name: "Bob",
+	last_name: "Dylan"
+};
+
+const output =  <div>
+                    <h1>My name is {customer.first_name}</h1>
+                    <h2>My last name is {customer.last_name}</h2>
+                </div>;
+
+ReactDOM.render(output, document.querySelector("#myDiv"));
