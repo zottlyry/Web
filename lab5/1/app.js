8942de5b1a3386748dad8d8966a8e58d59e79c89
zottlyry <mar1ya322@yandex.ru>
2025-06-21 00:40:34 +0300

    Delete lab5 directory

--- lab5/1/app.js
+++
@@ -1,9 +0,0 @@
-
-let age = "12";
-let name = "John";
-
-
-let output = <span>{name} is {age} years old</span>;
-
-
-ReactDOM.render(output, document.querySelector("#myDiv"));  
--- lab5/7/app.js
+++
@@ -1,5 +0,0 @@
-const PrintHello = () => {
-    return <h1>I Love React</h1>;
-}
-
-ReactDOM.render(PrintHello(), myDiv);
